18e9cf76725d51f3a7de9728470cfffd501ca118
Maciej Legas <s4922675@bournemouth.ac.uk>
2018-05-18 16:44:15 +0100

    Finished bowls.

---
+++ Assets/Scripts/ChangeColorIfHitBowl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorIfHitBowl : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Bowl")
+        {
+            collision.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+        }
+    }
+}
---
+++ Assets/Scripts/RotateBowlIfHit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBowlIfHit : MonoBehaviour
+{
+
+    public bool hit = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            hit = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hit && this.gameObject.transform.rotation.eulerAngles.x < 150)
+        {
+             this.gameObject.transform.Rotate(new Vector3(60, 0, 0) * Time.deltaTime);
+        }
+    }
+}
