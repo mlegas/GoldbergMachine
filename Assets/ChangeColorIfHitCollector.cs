7b480a1f6362ccaf79244b87a30bc8b4c3e2fc3f
Maciej Legas <s4922675@bournemouth.ac.uk>
2018-05-19 22:54:59 +0100

    Added a hatch.

---
+++ Assets/ChangeColorIfHitCollector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorIfHitCollector : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Collector")
+        {
+            collision.gameObject.GetComponent<Renderer>().material.color = Color.cyan;
+        }
+    }
+}
